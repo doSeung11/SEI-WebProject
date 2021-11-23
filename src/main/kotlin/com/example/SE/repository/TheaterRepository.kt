09ed5be931919ca08229db0090fbc895a9f9bf31
doSeung11 <a35393462@gmail.com>
2021-11-23 19:49:09 +0900

    theater service&controller 구현

--- src/main/kotlin/com/example/SE/repository/TheaterRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.SE.repository
-
-import com.example.SE.domain.Theater
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TheaterRepository : JpaRepository<Theater, Long>
--- src/main/kotlin/com/example/SE/repository/TimetableRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.SE.repository
-
-import com.example.SE.domain.Timetable
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TimetableRepository : JpaRepository<Timetable, Long>
