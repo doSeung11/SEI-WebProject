35280c0418dc92671771cf0986a7f1f90c30ec60
doSeung11 <a35393462@gmail.com>
2021-11-24 12:47:38 +0900

    [feat/back] domain class 오류 수정

--- src/main/kotlin/com/example/SE/repository/TimetableRepository.kt
+++ src/main/kotlin/com/example/SE/repository/TimetableRepository.kt
@@ -1,8 +1,8 @@
-//package com.example.SE.repository
-//
-//import com.example.SE.domain.Timetable
-//import org.springframework.data.repository.CrudRepository
-//import org.springframework.stereotype.Repository
-//
-//@Repository
-//interface TimetableRepository : CrudRepository<Timetable, Long>
+package com.example.SE.repository
+
+import com.example.SE.domain.Timetable
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TimetableRepository : CrudRepository<Timetable, Long>
