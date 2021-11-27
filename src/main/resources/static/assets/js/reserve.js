2076ad354ae6c22af39f3c8b4090d5be9f00d88f
AhnDo0 <51705063+AhnDo0@users.noreply.github.com>
2021-11-27 22:28:14 +0900

    [feat/back] 영화, 극장 선택 항목 form의 value값에 연동, 인원당 가격 계산 함수 구현

--- src/main/resources/static/assets/js/reserve.js
+++ src/main/resources/static/assets/js/reserve.js
@@ -1,21 +1,81 @@
-const movieTitle = document.querySelectorAll('.movie-button');
+const movie = document.querySelectorAll('.movie-button');
+const movieNo = document.querySelector('.movie-list-num');
 const theaterLocation= document.querySelectorAll('.theater-location');
 const theaterPlace = document.querySelectorAll('.theater-place');
 const reserveTimeWant = document.querySelectorAll('.reserve-time-button');
 const inputTitle = document.querySelector('.title');
+const inputSelectedMovie = document.querySelector('.movieNo');
 const inputSelectedTheater = document.querySelector('.selectedTheater');
 const inputRunningTime = document.querySelector('.runningTime')
 
+function count(type)  {
+  // 인원당 가격 결과를 표시할 element
+  const resultAdultElement = document.getElementById('result-adult');
+  const resultChildElement = document.getElementById('result-child');
+  const resultPreferElement = document.getElementById('result-prefer');
+  const costElement = document.getElementById('costResult');
+
+  // 현재 화면에 표시된 값
+  let numberAdult = resultAdultElement.innerText;
+  let numberChild = resultChildElement.innerText;
+  let numberPrefer = resultPreferElement.innerText;
+  let cost = costElement.innerText;
+
+  // 더하기/빼기
+    //성인
+  if(type == 'plusAdult') {
+    numberAdult = parseInt(numberAdult) + 1;
+    cost = parseInt(cost) + 12000;
+  }else if(type === 'minusAdult')  {
+    if(numberAdult>=1){
+      numberAdult = parseInt(numberAdult) - 1;
+      cost = parseInt(cost) - 12000;
+    }
+  }
+  else if(type == 'plusChild') {//어린이
+    numberChild = parseInt(numberChild) + 1;
+    cost = parseInt(cost) + 10000;
+  }else if(type == 'minusChild')  {
+    if(numberChild>=1){
+      numberChild = parseInt(numberChild) - 1;
+      cost = parseInt(cost) - 10000;
+    }
+  }else if(type == 'plusPrefer') {//우대
+    numberPrefer = parseInt(numberPrefer) + 1;
+    cost = parseInt(cost) + 8000;
+  }else if(type == 'minusPrefer')  {
+    if(numberPrefer>=1){
+      numberPrefer = parseInt(numberPrefer) - 1;
+      cost = parseInt(cost) - 8000;
+    }
+  }
+
+
+  // 결과 출력
+  resultAdultElement.innerText = numberAdult;
+  resultChildElement.innerText = numberChild;
+  resultPreferElement.innerText = numberPrefer;
+  costElement.innerText = cost;
+}
+
 //영화 선택시 배경색 등 바뀌도록
-movieTitle.forEach(list => {
+movie.forEach(list => {
     list.addEventListener('click', function() {
         const movieTitleWrapper = document.querySelectorAll('.movie-list-title-active');
         movieTitleWrapper.forEach(li => {
             li.classList.remove('movie-list-title-active');
         });
         list.classList.add('movie-list-title-active');
-        console.log(list.innerHTML);
-        inputTitle.value = list.innerHTML;
+
+
+        console.log(list.innerText);
+        console.log(list.innerText[list.innerText.length-4]+list.innerText[list.innerText.length-3]);
+
+        if(list.innerText[list.innerText.length-4] != '*'){
+           inputSelectedMovie.value = list.innerText[list.innerText.length-4]+list.innerText[list.innerText.length-3];
+        }else{
+            inputSelectedMovie.value = list.innerText[list.innerText.length-3];
+        }
     });
 });
 
@@ -43,8 +103,15 @@ theaterPlace.forEach(list => {
             li.classList.remove('theater-place-active');
         });
         list.classList.add('theater-place-active');
-        console.log(list.innerHTML);
-        inputSelectedTheater.value = list.innerHTML;
+        console.log(list.innerText);
+        console.log(list.innerText[list.innerText.length-2]+list.innerText[list.innerText.length-1]);
+
+        if(list.innerText[list.innerText.length-2] != '*'){
+           inputSelectedTheater.value = list.innerText[list.innerText.length-2]+list.innerText[list.innerText.length-1];
+        }else{
+            inputSelectedTheater.value = list.innerText[list.innerText.length-1];
+        }
+
     });
 });
 
