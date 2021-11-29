42cf7f304382ea9ab000d825e5fe177c4dbd8b69
doSeung11 <a35393462@gmail.com>
2021-11-30 01:56:46 +0900

    [feat/back] 관리자 페이지 영화 목록 불러오기

---
+++ src/main/kotlin/com/example/SE/service/AdminServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.SE.service
+
+import com.example.SE.domain.Movie
+import com.example.SE.domain.Theater
+import com.example.SE.repository.MovieRepository
+import com.example.SE.repository.TheaterRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class AdminServiceImpl constructor(@Autowired private val movieRepository: MovieRepository, private val theaterRepository: TheaterRepository): AdminService{
+    override fun getAllMovie(): List<Movie>? =
+            movieRepository.findAllBy();
+
+    override fun getMovie(id: Long): Movie? =
+            movieRepository.findById(id).orElse(null);
+
+    override fun getAllTheaters(): List<Theater>? =
+            theaterRepository.findAllBy();
+
+}
